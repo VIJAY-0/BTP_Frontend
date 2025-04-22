bb8bb5a360e470ffb680321b7a764293918c7ddc
Vijay <vijendrasinghbhati2002@gmail.com>
2025-04-22 18:25:20 +0530

    allow all hosts

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // exposes server to local network
-    port: 3000       // sets custom port
+    port: 3000,       // sets custom port
+    allowedHosts: 'all'  // <- allow all incoming hosts
   }
 })
