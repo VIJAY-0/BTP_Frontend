b50cebe95212f032f1888c582ffaa1f238600174
Vijay <vijendrasinghbhati2002@gmail.com>
2025-04-22 18:19:50 +0530

    viteconfic host 0000 port 3000

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // exposes server to local network
+    port: 3000       // sets custom port
+  }
 })
